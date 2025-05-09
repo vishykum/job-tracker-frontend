ffc20cd5bff926c998cd08693be8cc0dd5fdd9bc
Vishal <vishykum@ymail.com>
2025-05-09 00:04:06 -0700

    Fix base path for gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/job-tracker-frontend/">
       <App />
     </BrowserRouter>
   </StrictMode>,
