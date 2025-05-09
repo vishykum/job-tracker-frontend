60eed176042c874dfd06bb1cd054050f0e9ed75b
Vishal <vishykum@ymail.com>
2025-05-08 23:56:50 -0700

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "job-tracker-frontend",
+  base: "/job-tracker-frontend/",
   plugins: [
     react(),
     tailwindcss(),
